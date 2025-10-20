368f108a5ce098d2f4fbe77d6b7be669d5738220
shahir55312-collab <shahir55312@gmail.com>
2025-10-20 19:09:26 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,26 @@
+/*let cartquantity = 0;
+ const ADDCARTBUTT =               document.getElementById("ADDCARTBUTT");
+    ADDCARTBUTT.addEventListener("click", function() { 
+
+cartquantity += 1;
+console.log(`Cart Quantity: ${cartquantity}`);});
+    const ADD2 =               document.getElementById("ADD2");
+    ADD2.addEventListener("click", function() { 
+
+cartquantity = cartquantity*2;
+console.log(`Cart Quantity: ${cartquantity}`);});
+    const ADD3 =               document.getElementById("ADD3");
+    ADD3.addEventListener("click",function(){
+
+cartquantity += 3;
+console.log(`Cart Quantity: ${cartquantity}`);});
+const RESET =
+document.getElementById("RESET");
+RESET.addEventListener("click",function(){
+
+cartquantity=0;
+console.log(`Cart Quantity:${cartquantity}`)
+
+})*/
+
+ 
